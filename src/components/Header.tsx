5f8fdcb4b04c5cf9035b7471a378d6b08181635b
조경아 <kyoung_a_jo@naver.com>
2023-05-28 15:47:29 +0900

    'Routes'

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+function Header(){
+    return(
+        <div>여긴 헤더입니다</div>
+    )
+}
+
+export default Header;
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,8 @@
+
+function MainPage(){
+    return(
+        <div className="main_page">여긴 메인페이지입니다</div>
+    )
+}
+
+export default MainPage;
